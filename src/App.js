efc1c697f92c11c03afdd2101ec06d2f102aa833
greenwhale00 <goreakay@gmail.com>
2022-09-07 15:16:00 +0900

    220907

--- src/App.js
+++ src/App.js
@@ -1,11 +1,80 @@
 
+import { useState } from 'react';
 import './App.css';
 
+
+const DB = [
+  {
+    id: 1,
+    content: "menu01",
+    link: "/s1",
+    submenu: [
+      { content: "submenu011", link: "/1" },
+      { content: "submenu012", link: "/2" },
+      { content: "submenu013", link: "/3" },
+    ]
+  },
+  {
+    id: 2,
+    content: "menu02",
+    link: "/s1",
+    submenu: [
+      { content: "submenu021", link: "/1" },
+      { content: "submenu022", link: "/2" },
+      { content: "submenu023", link: "/3" },
+    ]
+  },
+  {
+    id: 3,
+    content: "menu03",
+    link: "/s1",
+    submenu: [
+      { content: "submenu031", link: "/1" },
+      { content: "submenu032", link: "/2" },
+      { content: "submenu033", link: "/3" },
+    ]
+  }
+]
+
+
+
+
 function App() {
+  const [CB, setCB] = useState("");
+  const [TG, setTG] = useState(false);
   return (
-    <>
-      README 가 왜 뜰까
-    </>
+    <header>
+      <h1 className={CB} onClick={() => setCB('on')}>LOGO</h1>
+      <h2 className={`ggg ${TG ? 'on' : ''}`}>toggle class</h2>
+      <button onClick={() => setTG(!TG)}>class 토글</button>
+      <nav className='GNB'>
+        <ul>
+          {
+            DB.map((it, idx) => (<li key={idx}>
+              <a href={it.link}>{it.content}</a>
+              <ul className='smenu'>
+                {
+                  it.submenu.map((smenu, idx) => <li key={idx}>
+                    <a href={smenu.link}>{smenu.content}</a>
+                  </li>)
+                }
+
+                {/* <li>
+              <a href="">
+                {it.submenu[0].content}
+              </a>
+            </li> */}
+              </ul>
+            </li>))
+          }
+
+        </ul>
+
+      </nav>
+
+    </header>
+
+
   );
 }
 
